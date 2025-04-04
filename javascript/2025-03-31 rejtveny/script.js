9f46fc206aa3025a10ded55d12d155cc8201a9e1
benedek.bence <benedek.bence@ckik.hu>
2025-04-04 10:29:00 +0200

    matrixbeolvas megirva yippie

--- javascript/2025-03-31 rejtveny/script.js
+++ javascript/2025-03-31 rejtveny/script.js
@@ -1,10 +1,36 @@
 
+function matrixKialakitas(sorok, k){
+    let matrix = [];
+    for(let i = k; i<k+10; i++){
+        let tomb = [];
+        console.log(sorok[i]);
+        let sor = sorok[i].split(" ");
+        for(let j = 0; j<10; j++){
+            tomb.push(parseInt(sor[j]));
+        }
+        matrix.push(tomb);
+    }
+    return matrix;
+}
 
-
+function adatokBeolvasasa(szoveg){
+    const sorok = szoveg.split(";");
+    //console.log(sorok);
+    const n = parseInt(sorok[0]);
+    let adatok = [];
+    for(let i = 1; i<sorok.length; i+=11){
+        const adat = {
+            nev: sorok[i],
+            matrix: matrixKialakitas(sorok, i+1)
+        }
+        adatok.push(adat);
+    }
+    return adatok;
+}
 
 function main(szoveg){
-    
-    let objfelbontas = adatokfelbontas(szoveg);
-
+    const adatok = adatokBeolvasasa(szoveg);
+    console.table(adatok);
 }
-main(szoveg)
+
+main(szoveg);
